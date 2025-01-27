51b09e45face103fb52787a6dd64ae2ee1e947c3
and1801 <72920393+and1801@users.noreply.github.com>
2025-01-27 15:24:21 +0800

    hometask

--- FirstDjangoProject/hometask/urls.py
+++ FirstDjangoProject/hometask/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new)
+    path('', views.index, name='home'),
+    path('newnewnew', views.new, name='page2'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
--- FirstDjangoProject/hometask/views.py
+++ FirstDjangoProject/hometask/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'hometask/index.html')
+
+    return render(request, 'hometask/index.html', {'caption':"TocaBoca"})
 
 def new(request):
     return render(request, 'hometask/new.html')
+
+def about(request):
+    return render(request, 'hometask/about.html')
+
+def services(request):
+    return render(request, 'hometask/services.html')
+
+def contact(request):
+    return render(request, 'hometask/contact.html')
